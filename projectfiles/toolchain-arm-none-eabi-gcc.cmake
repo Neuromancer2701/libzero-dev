05158a79309621a6db8951ba958236c398346e18
Seth King <king.seth@gmail.com>
2015-08-05 06:44:22 -0400

    renamed the scripts dir, added openocd

--- projectfiles/toolchain-arm-none-eabi-gcc.cmake
+++ projectfiles/toolchain-arm-none-eabi-gcc.cmake
@@ -4,20 +4,12 @@ set(CMAKE_SYSTEM_PROCESSOR cortex-m0plus)
 set(TARGET_TRIPLET "arm-none-eabi")
 
  
-find_program(ARM_CC ${TARGET_TRIPLET}-gcc
-    ${TOOLCHAIN_DIR}/bin
-    )
-find_program(ARM_CXX ${TARGET_TRIPLET}-g++
-    ${TOOLCHAIN_DIR}/bin
-    )
-find_program(ARM_OBJCOPY ${TARGET_TRIPLET}-objcopy
-    ${TOOLCHAIN_DIR}/bin
-    )
-find_program(ARM_SIZE_TOOL ${TARGET_TRIPLET}-size
-    ${TOOLCHAIN_DIR}/bin)
-
-find_program(ARM_STRIP_TOOL ${TARGET_TRIPLET}-strip
-    ${TOOLCHAIN_DIR}/bin)
+find_program(ARM_CC ${TARGET_TRIPLET}-gcc ${TOOLCHAIN_DIR}/bin)
+find_program(ARM_CXX ${TARGET_TRIPLET}-g++ ${TOOLCHAIN_DIR}/bin)
+find_program(ARM_OBJCOPY ${TARGET_TRIPLET}-objcopy ${TOOLCHAIN_DIR}/bin)
+find_program(ARM_SIZE_TOOL ${TARGET_TRIPLET}-size ${TOOLCHAIN_DIR}/bin)
+find_program(ARM_STRIP_TOOL ${TARGET_TRIPLET}-strip ${TOOLCHAIN_DIR}/bin)
+find_program(OPENOCD openocd /opt/openocd/bin)
  
 CMAKE_FORCE_C_COMPILER(${ARM_CC} GNU)
 CMAKE_FORCE_CXX_COMPILER(${ARM_CXX} GNU)
