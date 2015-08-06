9749f795ca2f1169c73d729b0d701a32ef626ae3
Seth King <king.seth@gmail.com>
2015-08-05 23:07:33 -0400

    The debugger connects but doesn't really do anything.  Going to add some libraries to created a better test program.

--- projectfiles/toolchain-arm-none-eabi-gcc.cmake
+++ projectfiles/toolchain-arm-none-eabi-gcc.cmake
@@ -1,22 +1,24 @@
 include(CMakeForceCompiler)
+
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR cortex-m0plus)
-set(TARGET_TRIPLET "arm-none-eabi")
+set(TARGET_PREFIX "arm-none-eabi")
 
  
-find_program(ARM_CC ${TARGET_TRIPLET}-gcc ${TOOLCHAIN_DIR}/bin)
-find_program(ARM_CXX ${TARGET_TRIPLET}-g++ ${TOOLCHAIN_DIR}/bin)
-find_program(ARM_OBJCOPY ${TARGET_TRIPLET}-objcopy ${TOOLCHAIN_DIR}/bin)
-find_program(ARM_SIZE_TOOL ${TARGET_TRIPLET}-size ${TOOLCHAIN_DIR}/bin)
-find_program(ARM_STRIP_TOOL ${TARGET_TRIPLET}-strip ${TOOLCHAIN_DIR}/bin)
+find_program(ARM_CC ${TARGET_PREFIX}-gcc ${TOOLCHAIN_DIR}/bin)
+find_program(ARM_CXX ${TARGET_PREFIX}-g++ ${TOOLCHAIN_DIR}/bin)
+find_program(ARM_OBJCOPY ${TARGET_PREFIX}-objcopy ${TOOLCHAIN_DIR}/bin)
+find_program(ARM_SIZE_TOOL ${TARGET_PREFIX}-size ${TOOLCHAIN_DIR}/bin)
+find_program(ARM_STRIP_TOOL ${TARGET_PREFIX}-strip ${TOOLCHAIN_DIR}/bin)
+find_program(ARM_GDB ${TARGET_PREFIX}-gdb ${TOOLCHAIN_DIR}/bin)
 find_program(OPENOCD openocd /opt/openocd/bin)
  
 CMAKE_FORCE_C_COMPILER(${ARM_CC} GNU)
 CMAKE_FORCE_CXX_COMPILER(${ARM_CXX} GNU)
 
 
-set(TOOLCHAIN_INC_DIR ${TOOLCHAIN_DIR}/${TARGET_TRIPLET}/include)
-set(TOOLCHAIN_LIB_DIR ${TOOLCHAIN_DIR}/${TARGET_TRIPLET}/lib)
+set(TOOLCHAIN_INC_DIR ${TOOLCHAIN_DIR}/${TARGET_PREFIX}/include)
+set(TOOLCHAIN_LIB_DIR ${TOOLCHAIN_DIR}/${TARGET_PREFIX}/lib)
 
 set(CMAKE_C_FLAGS
   "${CMAKE_C_FLAGS}"
